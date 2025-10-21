4a9d21d9bfb302de9136591062aad4b688b9e7d0
okonkwo348 <okonkwoemmanuel348@gmail.com>
2025-10-21 14:32:19 +0100

    updated view, urls, settings.py, requirements.txt

--- profileapp/urls.py
+++ profileapp/urls.py
@@ -1,6 +1,6 @@
-from .views import profile_view
 from django.urls import path
+from .views import profile_view
 
-urlpatterns= [
-    path("me/", profile_view, name='profile'),
+urlpatterns = [
+    path('me/', profile_view, name='profile'),
 ]
--- profileapp/views.py
+++ profileapp/views.py
@@ -1,30 +1,29 @@
 import requests
-from rest_framework.response import Response
-from rest_framework.decorators import api_view
 from datetime import datetime, timezone
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 @api_view(['GET'])
 def profile_view(request):
-    try:
-        response=requests.get("https://catfact.ninja/fact", timeout=5)
-        response.raise_for_status()
-        cat_data = response.join()
-        cat_fact = cat_data.get("fact", "Cats are wonderful creature!")
-    except requests.exceptions.RequestException:
-        cat_fact = "Could not fetch a cat fact right now, please try again later."
+    """Stage 0 Task: /me endpoint — returns profile info and a dynamic cat fact."""
 
-    current_time = datetime.now(timezone.utc).isoformat().replace("+00:00", "Z")
+    cat_fact_url = "https://catfact.ninja/fact"
+    try:
+        response = requests.get(cat_fact_url, timeout=5)
+        response.raise_for_status()  # ensures non-200 raises an error
+        cat_fact = response.json().get("fact", "Cats are awesome!")
+    except requests.exceptions.RequestException:
+        cat_fact = "Cat fact service is currently unavailable."
 
     data = {
-        "status":"success",
-        "user":{
-            "email":"okonkwopaul348@.com",
-            "name":"Okonkwo Emmanuel",
-            "stack":"Python/Django"
+        "status": "success",
+        "user": {
+            "email": "okonkwoemmanuel348@gmail.com",
+            "name": "Okonkwo Emmanuel",
+            "stack": "Python/Django"
         },
-        "timestamp":current_time,
+        "timestamp": datetime.now(timezone.utc).isoformat().replace("+00:00", "Z"),
         "fact": cat_fact
     }
 
     return Response(data)
-
